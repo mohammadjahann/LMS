2d07a798c4b74eef06e3248b701ef03ffb11705d
Jahan <you@example.com>
2026-02-17 22:47:56 +0330

    style: add fonts and extra screen size to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'esm': '328px',
+        'msm': '500px'
+      },
+
+      fontFamily: {
+        'MTNIrancell-Bold': 'MTNIrancell-Bold',
+        'MTNIrancell-DemiBold': 'MTNIrancell-DemiBold',
+        'MTNIrancell-ExtraLight': 'MTNIrancell-ExtraLight',
+        'MTNIrancell-Light': 'MTNIrancell-Light',
+        'MTNIrancell-Medium': 'MTNIrancell-Medium',
+        'Oi-Regular': 'Oi-Regular',
+      },
+    },
   },
   plugins: [],
 }
