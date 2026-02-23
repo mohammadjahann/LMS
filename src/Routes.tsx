93e0f4796544aa64b5be4d07470af93521cb04bc
Jahan <you@example.com>
2026-02-23 11:17:19 +0330

    feat(Routes) : Add StudentLogIn , StudentProtected and EducatorLogIn Routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -9,21 +9,35 @@ import Educator from "./pages/educator/Educator";
 import Dashboard from "./pages/educator/Dashboard";
 import AddCourse from "./pages/educator/AddCourse";
 import MyStudents from "./pages/educator/MyStudents";
+import StudentProtected from "./pages/students/StudentProtected";
+import DashBoard from "./pages/students/DashBoard";
+import StudentsLogIn from "./pages/students/StudentsLogIn";
+import EducatorLogIn from "./pages/educator/EducatorLogIn";
 
-const Routes : RouteObject[] = [
-    {path:'/', element:<Home/>},
-    {path:'/courses-list', element:<CoursesList/>},
-    {path:'/courses-list/:input', element:<CoursesList/>},
-    {path:'/course/:id', element:<CourseDetails/>},
-    {path:'/my-curses', element:<MyCurses/>},
-    {path:'/player/:courseId', element:<Player/>},
-    {path:'/loading/:path', element:<Loading/>},
-    {path:'/educator', element:<Educator/> , children :[
-        {path : 'educator' , element : <Dashboard/>},
-        {path : 'add-course' , element : <AddCourse/>},
-        {path : 'my-courses' , element : <MyCurses/>},
-        {path : 'my-students' , element : <MyStudents/>},
-    ]},
-] 
+const Routes: RouteObject[] = [
+    { path: '/', element: <Home /> },
+    { path: '/courses-list', element: <CoursesList /> },
+    { path: '/courses-list/:input', element: <CoursesList /> },
+    {path:'students-login', element:<StudentsLogIn/>},
+    {path:'educator-login', element:<EducatorLogIn/>},
+    {
+        path: '/student', element: <StudentProtected />, children: [
+            { path: '/my-curses', element: <MyCurses /> },
+            { path: '/dashbord', element: <DashBoard /> },
+        ]
+    },
+    { path: '/course/:id', element: <CourseDetails /> },
+
+    { path: '/player/:courseId', element: <Player /> },
+    { path: '/loading/:path', element: <Loading /> },
+    {
+        path: '/educator', element: <Educator />, children: [
+            { path: 'educator', element: <Dashboard /> },
+            { path: 'add-course', element: <AddCourse /> },
+            { path: 'my-courses', element: <MyCurses /> },
+            { path: 'my-students', element: <MyStudents /> },
+        ]
+    },
+]
 
 export default Routes
