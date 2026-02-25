a24825080a19569552b6070d4f0e76de7ce6e31d
Jahan <you@example.com>
2026-02-25 14:23:55 +0330

    style(AuthInput): change input styles

--- src/components/students/LogIn/AuthInput.tsx
+++ src/components/students/LogIn/AuthInput.tsx
@@ -13,7 +13,7 @@ const AuthInput = ({ label, name, type, value, formChangeHandler }: AuthInputPro
             <label className=" text-[12px]">{label}</label>
             <input
                 name={name}
-                className=" w-[70%] h-8 bg-slate-200 text-slate-600 outline-none rounded-md text-right"
+                className=" w-full h-8 bg-slate-200 text-slate-600 outline-none rounded-md text-right focus:bg-slate-300"
                 type={type}
                 value={value}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => formChangeHandler(e)} />
