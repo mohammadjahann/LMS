f6ae404473630cea4463154e8ac92398fe982766
Jahan <you@example.com>
2026-02-26 17:32:03 +0330

    refactor(students-login):add login logic and add Success and Error Modals in LogInForm

--- src/components/students/LogIn/LogInForm.tsx
+++ src/components/students/LogIn/LogInForm.tsx
@@ -2,6 +2,10 @@ import React, { useState } from "react"
 import AuthInput from "./AuthInput"
 import AuthHeader from "./AuthHeader"
 import AuthFooter from "./AuthFooter"
+import { supabase } from "../../../supabase"
+import ErrorModal from "./ErrorModal"
+import SuccsessModal from "./SuccsessModal"
+import { useNavigate } from "react-router-dom"
 
 type props = {
     modeHandler: () => void
@@ -14,6 +18,16 @@ const LogInForm = ({ modeHandler }: props) => {
         password: ''
     })
 
+    const [showLoadScreen, setShowLoadScreen] = useState<boolean>(false)
+    const [showErrorModal, setShowErrorModal] = useState<boolean>(false)
+    const [errorText, setErrorText] = useState<string>('')
+    const [showSuccessModal, setShowSuccessModal] = useState<boolean>(false)
+
+    const navigate = useNavigate()
+
+
+    // Handle AuthInputs
+
     const formChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
         const { name, value } = e.target
 
@@ -23,12 +37,43 @@ const LogInForm = ({ modeHandler }: props) => {
         }))
     }
 
+    // LogIn Logic
+
+    const userLogInHandler = async () => {
+        setShowLoadScreen(true)
+
+        const { error } = await supabase.auth.signInWithPassword({
+            email: formData.email,
+            password: formData.password
+        })
+
+        if (error) {
+            setErrorText(error.message)
+            setShowErrorModal(true)
+        } else {
+            setShowSuccessModal(true)
+           
+            setTimeout(() => {
+                 navigate('/')
+            }, 3000);
+        }
+
+    }
+
+    const errorModalClose = () :void =>{
+        setShowLoadScreen(false)
+        setShowErrorModal(false)
+
+    }
+
     return (
         <>
-            <AuthHeader title="خوش حالیم که برگشتی" text="به اکانت خود وارد شوید تا به دنیای آموزش بروید"/>
-            
+            <AuthHeader title="خوش حالیم که برگشتی" text="به اکانت خود وارد شوید تا به دنیای آموزش بروید" />
 
-            <form className=" w-full text-right flex flex-col py-3 px-5 gap-3">
+
+            <form
+                className=" w-full text-right flex flex-col py-3 px-5 gap-3"
+                onSubmit={(e: React.SubmitEvent<HTMLFormElement>) => { e.preventDefault() }}>
 
                 <AuthInput
                     name="email"
@@ -45,12 +90,26 @@ const LogInForm = ({ modeHandler }: props) => {
                     formChangeHandler={formChangeHandler} />
 
                 <button
-                    className=" w-full bg-blue-300 py-2 rounded-lg">
+                    onClick={userLogInHandler}
+                    className=" w-full bg-blue-300 py-2 rounded-lg hover:bg-blue-400 hover:text-white transition-colors duration-200">
                     ورود
                 </button>
             </form>
 
-            <AuthFooter modeHandler={modeHandler} title=" اکانت ندارید ؟ " callToAction="ساخت اکانت"/>
+            <AuthFooter modeHandler={modeHandler} title=" اکانت ندارید ؟ " callToAction="ساخت اکانت" />
+
+            {/* Loading */}
+
+            {showLoadScreen && (<div className=" fixed inset-0 bg-black/50"></div>)}
+
+            {/* Error Modal */}
+
+            {showErrorModal && <ErrorModal errorText={errorText} errorModalClose={errorModalClose}/>}
+
+            {/* Success Modal */}
+
+            {showSuccessModal && <SuccsessModal text="ورود با موفقیت انجام شد" action="هم اکنون به صفحه اصلی هدایت میشوید"/>}
+
         </>
     )
 }
