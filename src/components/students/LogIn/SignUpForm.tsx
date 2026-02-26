260841afe629d6409030f21956306ecf566dc41b
Jahan <you@example.com>
2026-02-26 15:45:55 +0330

    refactor(students-signup) add signup logic from spubase

--- src/components/students/LogIn/SignUpForm.tsx
+++ src/components/students/LogIn/SignUpForm.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import AuthInput from "./AuthInput"
 import AuthHeader from "./AuthHeader"
 import AuthFooter from "./AuthFooter"
+import { supabase } from "../../../supabase"
 
 type props = {
     modeHandler: () => void
@@ -15,6 +16,8 @@ const SignUpForm = ({ modeHandler }: props) => {
         tel: ''
     })
 
+    const [loadScreen, setLoadScreen] = useState<boolean>(true)
+
     const formChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
 
         const { name, value } = e.target
@@ -25,13 +28,34 @@ const SignUpForm = ({ modeHandler }: props) => {
         }))
     }
 
+    const signUpHandler = async () => {
+        const { email, password } = formData
+        const { data, error } = await supabase.auth.signUp({
+            email,
+            password,
+            phone: formData.tel
+        })
+
+
+
+        if (error) {
+            console.log("ERROR:", error.message)
+        } else {
+            console.log("USER CREATED:", data)
+            alert("ثبت نام موفق 🎉")
+        }
+
+    }
+
     return (
         <>
 
             <AuthHeader title="خوش حالیم که قراره به ما بپیوندی" text="حسابی بسازید تا از دنیای اموزش های ما بهره مند شوید" />
 
 
-            <form className=" w-full text-right flex flex-col py-3 px-5 gap-3">
+            <form
+                onSubmit={(e: React.SubmitEvent<HTMLFormElement>) => e.preventDefault()}
+                className=" w-full text-right flex flex-col py-3 px-5 gap-3">
 
                 <AuthInput
                     name="email"
@@ -55,13 +79,17 @@ const SignUpForm = ({ modeHandler }: props) => {
                     formChangeHandler={formChangeHandler} />
 
                 <button
-                    className=" w-full bg-blue-300 py-2 rounded-lg">
-                    ورود
+                    onClick={signUpHandler}
+                    className=" w-full bg-blue-300 py-2 rounded-lg hover:bg-blue-400 hover:text-white transition-colors duration-200">
+                    ثبت نام
                 </button>
             </form>
 
-            <AuthFooter modeHandler={modeHandler} title="اکانت دارید ؟" callToAction="وارد شوید"/>
-           
+            <AuthFooter modeHandler={modeHandler} title="اکانت دارید ؟" callToAction="وارد شوید" />
+
+            {/* Loading */}
+
+            {loadScreen && <div className=" fixed inset-0 bg-black/50"></div>}
         </>
     )
 }
