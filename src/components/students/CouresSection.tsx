c4a90cba3a6717f7b78143ce826fb38a25125d10
Jahan <you@example.com>
2026-03-07 17:25:39 +0330

    feat(studebts-Home):implement initial CourseCard component and use it in CourseSection component

--- src/components/students/CouresSection.tsx
+++ src/components/students/CouresSection.tsx
@@ -1,12 +1,85 @@
 import { Link } from "react-router-dom"
+import CourseCard from "./CourseCard"
+
+const coursesData = [
+
+ 
+  {
+    courseTitle: 'Pro React',
+    author: 'Jahan abadi',
+    description: 'text',
+    rate: 4.5,
+    price: 1800000,
+    courseBanner: './src/assets/course_1.png',
+    seassens: [
+      {
+        seassenTitle: 'base', seassenChapters: [
+          { chapterTitle: 'install react', chapterSrc: 'https:asd', iswatched: false },
+          { chapterTitle: 'base react structure', chapterSrc: 'https:asd', iswatched: false },
+        ]
+      },
+      {
+        seassenTitle: 'how to code', seassenChapters: [
+          { chapterTitle: 'jsx', chapterSrc: 'https:asd', iswatched: false },
+          { chapterTitle: 'condetinalrendering', chapterSrc: 'https:asd', iswatched: false },
+        ]
+      },
+    ]
+  },
+  {
+    courseTitle: 'Pro React',
+    author: 'Jahan abadi',
+    description: 'text',
+    rate: 4.5,
+    price: 1800000,
+    courseBanner: './src/assets/course_1.png',
+    seassens: [
+      {
+        seassenTitle: 'base', seassenChapters: [
+          { chapterTitle: 'install react', chapterSrc: 'https:asd', iswatched: false },
+          { chapterTitle: 'base react structure', chapterSrc: 'https:asd', iswatched: false },
+        ]
+      },
+      {
+        seassenTitle: 'how to code', seassenChapters: [
+          { chapterTitle: 'jsx', chapterSrc: 'https:asd', iswatched: false },
+          { chapterTitle: 'condetinalrendering', chapterSrc: 'https:asd', iswatched: false },
+        ]
+      },
+    ]
+  },
+  {
+    courseTitle: 'Pro React',
+    author: 'Jahan abadi',
+    description: 'text',
+    rate: 4.5,
+    price: 1800000,
+    courseBanner: './src/assets/course_1.png',
+    seassens: [
+      {
+        seassenTitle: 'base', seassenChapters: [
+          { chapterTitle: 'install react', chapterSrc: 'https:asd', iswatched: false },
+          { chapterTitle: 'base react structure', chapterSrc: 'https:asd', iswatched: false },
+        ]
+      },
+      {
+        seassenTitle: 'how to code', seassenChapters: [
+          { chapterTitle: 'jsx', chapterSrc: 'https:asd', iswatched: false },
+          { chapterTitle: 'condetinalrendering', chapterSrc: 'https:asd', iswatched: false },
+        ]
+      },
+    ]
+  },
+
+]
 
 
 const CouresSection = () => {
 
   return (
 
-    <div 
-    className="flex flex-col gap-4 py-16 px-8 md:px-40 justify-center items-center">
+    <div
+      className="flex flex-col gap-4 py-16 px-8 md:px-40 justify-center items-center bg-gradient-to-t from-cyan-100/70">
 
       <h2
         className="text-3xl font-MTNIrancell-Light text-gray-800">
@@ -17,6 +90,16 @@ const CouresSection = () => {
         className="text-sm md:text-base text-gray-500 mt-3">
         بهترین و تاپ ریت ترین دوره های موجود را در زمینه های متفاوت دریافت کنید .از برنامه نویسی گرفته تا دوره های مربوط به بیزنس شخصی ، دوره های ما برای بهترین نتایج اماده سازی شده اند
       </p>
+
+      <div
+        className="flex gap-4 items-center justify-center w-full flex-wrap ">
+        {coursesData.map((course, i) => (
+
+          <CourseCard id={i} author={course.author} src={course.courseBanner} rating={course.rate} title={course.courseTitle} price={course.price}/>
+
+        ))}
+
+      </div>
 
       <Link
         to={'/courses-list'}
--- src/components/students/CourseCard.tsx
+++ src/components/students/CourseCard.tsx
@@ -1,7 +1,47 @@
+import { Link } from "react-router-dom"
 
-const CourseCard = () => {
+type courseCardProps = {
+  id: number,
+  src: string,
+  rating: number,
+  title: string,
+  author: string,
+  price: number
+}
+
+const CourseCard = ({ id, src, rating, title, author , price}: courseCardProps) => {
   return (
-    <div>CourseCard</div>
+    <div
+    className=" w-[90%] sm:w-[45%] md:w-[30%] border shadow-sm rounded-md overflow-hidden"
+    key={id}>
+
+      <img src={src} alt="" />
+
+      <div 
+      className=" p-3 flex flex-col ">
+
+        <h3>{title}</h3>
+
+        <p>{author}</p>
+
+        <div
+        className=" flex justify-between">
+
+          <p>${price}</p>
+
+          <p>{rating}</p>
+
+        </div>
+
+        <Link
+         to={`/course/${id}`}
+         className=" bg-blue-400 py-2 px-3 rounded-sm hover:bg-blue-500 hover:text-white transition-colors duration-200"
+        >جزئیات بیشتر</Link>
+
+      </div>
+
+
+    </div>
   )
 }
 
