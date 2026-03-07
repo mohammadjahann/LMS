0b680815c7e511cf3e754f0f0621a370169f4db3
Jahan <you@example.com>
2026-03-07 18:19:58 +0330

    feat(studebts-Home):implement initial CallToAction component and use it in Home page

--- src/components/students/CallToAction.tsx
+++ src/components/students/CallToAction.tsx
@@ -2,7 +2,36 @@
 
 const CallToAction = () => {
   return (
-    <div>CallToAction</div>
+    <div
+      className=" bg-gradient-to-b bg-cyan-100/70 w-full flex flex-col items-center gap-4 pt-10 pb-24 px-8 md:px-0">
+
+      <h1
+        className=" text-xl md:text-4xl text-gray-800 font-MTNIrancell-DemiBold">
+        یاد گیری مداوم ، هر زمان ، هر کجا
+      </h1>
+
+      <p
+        className=" text-gray-500 sm:text-sm font-MTNIrancell-Light">
+        آموزش انلاین به همراه پشتیبانی مداوم از دوره ها ، امکان یادگیری با راندمان بالا را فراهم میکند تا شما بهنترین نتایج را بگیرید
+      </p>
+
+      <div
+        className="flex items-center font-MTNIrancell-Medium gap-6 mt-4">
+
+        <button
+        className=" px-10 py-3 rounded-md text-white bg-blue-600">
+          شروع کنید
+        </button>
+
+        <button
+        className="flex items-center gap-2">
+          جزئیات بیشتر <img src="./src/assets/arrow_icon.svg" alt="arrow icon" />
+          </button>
+
+      </div>
+
+
+    </div>
   )
 }
 
--- src/pages/students/Home.tsx
+++ src/pages/students/Home.tsx
@@ -1,3 +1,4 @@
+import CallToAction from "../../components/students/CallToAction"
 import Companies from "../../components/students/Companies"
 import CouresSection from "../../components/students/CouresSection"
 import Hero from "../../components/students/Hero"
@@ -9,11 +10,12 @@ const Home = () => {
 
   return (
     <div
-      className="flex flex-col items-center space-y-7 text-center">
+      className="flex flex-col items-center  text-center">
 
       <Hero />
       <Companies />
       <CouresSection/>
+      <CallToAction/>
 
     </div>
   )
