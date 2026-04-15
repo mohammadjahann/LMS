7091b3c27f71e529cf4e515d8e0e89e072a7be28
Jahan <you@example.com>
2026-04-15 13:01:45 +0330

    [UPDATE] change students Navbar components styles

--- src/components/students/Navbar.tsx
+++ src/components/students/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
 
 
-    const isCourseListPage: boolean = location.pathname.includes('/courses-list')
+
 
     const isActive = ({ isActive }: { isActive: boolean }) => {
         return isActive ? 'border-b-2 border-lime-500 animate-fill-border' : '';
@@ -33,7 +33,7 @@ const Navbar = () => {
 
     return (
         <nav
-            className={`font-MTNIrancell-DemiBold flex  flex-row-reverse items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? ' bg-white' : 'bg-cyan-100/70'}`}>
+            className={`font-MTNIrancell-DemiBold flex  flex-row-reverse items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 bg-cyan-100/70`}>
 
             {/* Logo */}
             <Link to={'/'}>
@@ -81,11 +81,27 @@ const Navbar = () => {
             {/* Side menu */}
 
             <div
-             className={`absolute flex flex-col  items-center pt-4 gap-3 border-r-2 top-0 bottom-0 left-0 w-[50%] esm:w-[30%] bg-black/25 z-20 ${showSideMenu ? 'translate-x-0':'-translate-x-full'} transition-transform duration-500 ease-in-out text-white`}>
+                className={`absolute flex flex-col  items-center pt-4 gap-3 border-r-2 top-0 bottom-0 left-0 w-[60%] esm:w-[40%] bg-black/25 z-20 ${showSideMenu ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-500 ease-in-out text-white`}>
                 <RxCross2
-                className=" text-xl esm:text-2xl"
-                onClick={()=>setShowSideMenu(false)} />
+                    className=" text-xl esm:text-2xl"
+                    onClick={() => setShowSideMenu(false)} />
                 {menu('flex flex-col text-center text-[15px] esm:text-[17px]')}
+                <div
+                    className=" flex flex-col-reverse justify-center items-center gap-9 text-gray-500">
+
+                    <Link
+                        to={'educator-login'}
+                        className=" border-white/20 border text-white px-3 py-2 rounded-lg">
+                        ورود مدرس
+                    </Link>
+
+                    <Link
+                        to={'students-login'}
+                        className=" bg-blue-600 text-white px-5 py-2 rounded-full">
+                        ساخت اکانت
+                    </Link>
+
+                </div>
 
             </div>
 
