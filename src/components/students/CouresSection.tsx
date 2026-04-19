f61e6552868854866cd9a361df597599d3c74326
Jahan <you@example.com>
2026-04-19 14:31:34 +0330

    [REFACTOR] change CourseCard Component and use it it CourseSection

--- src/components/students/CouresSection.tsx
+++ src/components/students/CouresSection.tsx
@@ -1,77 +1,11 @@
 import { Link } from "react-router-dom"
 import CourseCard from "./CourseCard"
+import { dummyCourses } from "../../assets/assets"
+import type { CourseType } from "../../Types"
 
-const coursesData = [
+const coursesData: CourseType[] = dummyCourses
 
- 
-  {
-    courseTitle: 'Pro React',
-    author: 'Jahan abadi',
-    description: 'text',
-    rate: 4.5,
-    price: 1800000,
-    courseBanner: './src/assets/course_1.png',
-    seassens: [
-      {
-        seassenTitle: 'base', seassenChapters: [
-          { chapterTitle: 'install react', chapterSrc: 'https:asd', iswatched: false },
-          { chapterTitle: 'base react structure', chapterSrc: 'https:asd', iswatched: false },
-        ]
-      },
-      {
-        seassenTitle: 'how to code', seassenChapters: [
-          { chapterTitle: 'jsx', chapterSrc: 'https:asd', iswatched: false },
-          { chapterTitle: 'condetinalrendering', chapterSrc: 'https:asd', iswatched: false },
-        ]
-      },
-    ]
-  },
-  {
-    courseTitle: 'Pro React',
-    author: 'Jahan abadi',
-    description: 'text',
-    rate: 4.5,
-    price: 1800000,
-    courseBanner: './src/assets/course_1.png',
-    seassens: [
-      {
-        seassenTitle: 'base', seassenChapters: [
-          { chapterTitle: 'install react', chapterSrc: 'https:asd', iswatched: false },
-          { chapterTitle: 'base react structure', chapterSrc: 'https:asd', iswatched: false },
-        ]
-      },
-      {
-        seassenTitle: 'how to code', seassenChapters: [
-          { chapterTitle: 'jsx', chapterSrc: 'https:asd', iswatched: false },
-          { chapterTitle: 'condetinalrendering', chapterSrc: 'https:asd', iswatched: false },
-        ]
-      },
-    ]
-  },
-  {
-    courseTitle: 'Pro React',
-    author: 'Jahan abadi',
-    description: 'text',
-    rate: 4.5,
-    price: 1800000,
-    courseBanner: './src/assets/course_1.png',
-    seassens: [
-      {
-        seassenTitle: 'base', seassenChapters: [
-          { chapterTitle: 'install react', chapterSrc: 'https:asd', iswatched: false },
-          { chapterTitle: 'base react structure', chapterSrc: 'https:asd', iswatched: false },
-        ]
-      },
-      {
-        seassenTitle: 'how to code', seassenChapters: [
-          { chapterTitle: 'jsx', chapterSrc: 'https:asd', iswatched: false },
-          { chapterTitle: 'condetinalrendering', chapterSrc: 'https:asd', iswatched: false },
-        ]
-      },
-    ]
-  },
 
-]
 
 
 const CouresSection = () => {
@@ -88,14 +22,14 @@ const CouresSection = () => {
 
       <p
         className="text-sm md:text-base text-gray-500 mt-3">
-        بهترین و تاپ ریت ترین دوره های موجود را در زمینه های متفاوت دریافت کنید .از برنامه نویسی گرفته تا دوره های مربوط به بیزنس شخصی ، دوره های ما برای بهترین نتایج اماده سازی شده اند
+        بهترین و تاپ ریت ترین دوره های موجود را در زمینه های متفاوت دریافت کنید <br />از برنامه نویسی گرفته تا دوره های مربوط به بیزنس شخصی ، دوره های ما برای بهترین نتایج اماده سازی شده اند
       </p>
 
       <div
-        className="flex gap-4 items-center justify-center w-full flex-wrap ">
-        {coursesData.map((course, i) => (
+        className=" w-full flex items-center justify-center gap-2 flex-wrap">
+        {coursesData.slice(0, 4).map((course) => (
 
-          <CourseCard id={i} author={course.author} src={course.courseBanner} rating={course.rate} title={course.courseTitle} price={course.price}/>
+          <CourseCard courseData={course} key={course._id} />
 
         ))}
 
--- src/components/students/CourseCard.tsx
+++ src/components/students/CourseCard.tsx
@@ -1,46 +1,82 @@
+// import { Link } from "react-router-dom"
 import { Link } from "react-router-dom"
+import { assets } from "../../assets/assets"
+import type { CourseType } from "../../Types"
+import { MdAddShoppingCart } from "react-icons/md"
 
-type courseCardProps = {
-  id: number,
-  src: string,
-  rating: number,
-  title: string,
-  author: string,
-  price: number
+type Props = {
+  courseData: CourseType,
+
 }
 
-const CourseCard = ({ id, src, rating, title, author , price}: courseCardProps) => {
+const CourseCard = ({ courseData }: Props) => {
+
   return (
     <div
-    className=" w-[90%] sm:w-[45%] md:w-[30%] border shadow-sm rounded-md overflow-hidden"
-    key={id}>
+      className=" flex flex-col gap-2 items-end justify-between border border-gray-500/30 overflow-hidden pb-4 rounded-lg w-[80%] esm:w-[65%] md:w-[45%] lg:w-[24%] h-[350px]">
 
-      <img src={src} alt="" />
+      <img
+        className="w-full max-h-[200px]"
+        src={courseData.courseThumbnail} alt="Thumbnail" />
+      <div
+        className=" flex flex-col h-full w-full items-end justify-between pr-3  text-right">
 
-      <div 
-      className=" p-3 flex flex-col ">
+        <h3
+          className=" text-base font-semibold">
 
-        <h3>{title}</h3>
+          {courseData.courseTitle}
 
-        <p>{author}</p>
+        </h3>
+
+        <p
+          className=" text-gray-500">
+
+          {courseData.educator}
+
+        </p>
 
         <div
-        className=" flex justify-between">
+          className=" flex items-center space-x-2">
 
-          <p>${price}</p>
+          <p>4.5</p>
 
-          <p>{rating}</p>
+          <div className="flex">
+
+            {[...Array(5)].map((_, i) => (
+              <img key={i} src={assets.star} alt="" className=" w-3.5 h-3.5" />
+            ))}
+
+          </div>
+
+          <p
+            className=" text-gray-500">
+            22
+          </p>
 
         </div>
 
-        <Link
-         to={`/course/${id}`}
-         className=" bg-blue-400 py-2 px-3 rounded-sm hover:bg-blue-500 hover:text-white transition-colors duration-200"
-        >جزئیات بیشتر</Link>
+        <p
+          className="  text-base gap-2 font-semibold text-gray-800 font-MTNIrancell-DemiBold">
+
+          {(courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toLocaleString('fa-IR')}
+          <span>تومان</span>
+
+        </p>
+
+        <div
+          className=" w-full flex flex-row-reverse items-center gap-1">
+          <Link
+            className=" w-[85%] py-2 bg-blue-400 text-center rounded-md text-white hover:bg-blue-600 transition-colors duration-300"
+            to={`/course/${courseData._id}`}
+            onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+            اطلاعات بیشتر
+          </Link>
+          <button>
+            <MdAddShoppingCart className=" text-blue-400 hover:text-blue-600 transition-colors duration-300" size={24} />
+          </button>
+        </div>
 
       </div>
-
-
     </div>
   )
 }
