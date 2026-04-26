a3ac3a2d85918a938bee8ccab395766e4eb836b8
Jahan <you@example.com>
2026-04-26 14:30:19 +0330

    [ADD]:define chapterDurationCalculator and courseDurationCalculator and humanizeTime functions in AppContext

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,10 +1,15 @@
 import { createContext, useState, type Dispatch, type ReactNode, type SetStateAction } from "react";
-import type { CourseRatings } from "../Types";
+import type { ChapterContent, CourseContent, CourseRatings } from "../Types";
+import humanizrDuration from "humanize-duration"
 
 type AppContextType = {
     isDark: boolean,
-    setIsDark: Dispatch<SetStateAction<boolean>>
-    ratingCalculator: (allRatings: CourseRatings[]) => number
+    setIsDark: Dispatch<SetStateAction<boolean>>,
+    ratingCalculator: (allRatings: CourseRatings[]) => number,
+    humanizrDuration: (time: number) => string,
+    chapterDurationCalculator: (chapters: ChapterContent[]) => number,
+    courseDurationCalculator: (courseContent: CourseContent[]) => string
+
 }
 
 const AppContext = createContext<null | AppContextType>(null)
@@ -14,6 +19,8 @@ type Props = {
 }
 
 const AppContextProvider = ({ children }: Props) => {
+
+    // function to calculate average rating of course
 
     const ratingCalculator = (allRatings: CourseRatings[]): number => {
         if (allRatings.length === 0) return 0;
@@ -30,10 +37,35 @@ const AppContextProvider = ({ children }: Props) => {
 
     }
 
+    //Humanize time
+    const humanizeTime = (time: number): string => {
+
+        return humanizrDuration(time * 60 * 1000, { units: ['h', 'm'] })
+
+    }
+
+    //Function to calculate course chapter time
+    const chapterDurationCalculator = (chapters: ChapterContent[]): number => {
+        let time = 0
+
+        chapters.map((lecture: ChapterContent) => time += lecture.lectureDuration)
+
+        return time
+    }
+
+    // Function to calculate course duration 
+
+    const courseDurationCalculator = (courseContent: CourseContent[]): string => {
+        let time = 0
+        courseContent.forEach(course => time += chapterDurationCalculator(course.chapterContent))
+
+        return humanizeTime(time)
+    }
+
     const [isDark, setIsDark] = useState(false)
 
     return (
-        <AppContext.Provider value={{ isDark, setIsDark, ratingCalculator }}>
+        <AppContext.Provider value={{ isDark, setIsDark, ratingCalculator, humanizrDuration, chapterDurationCalculator, courseDurationCalculator }}>
             {children}
         </AppContext.Provider>
     )
