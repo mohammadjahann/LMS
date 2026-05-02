326fc726e05bee938b9d2a3f2819256102ded531
Jahan <you@example.com>
2026-05-02 14:29:44 +0330

    [REFACTOR] : define Player Component

--- src/pages/students/Player.tsx
+++ src/pages/students/Player.tsx
@@ -1,8 +1,29 @@
+import type { ChapterContent } from "../../Types"
 
 
-const Player = () => {
+type PlayerProps = {
+  lectureData: ChapterContent
+}
+
+const Player = ({ lectureData }: PlayerProps) => {
+
   return (
-    <div>Player</div>
+    <div
+      className="dir flex flex-col w[90%] pr-3 pt-4 gap-2">
+      <h3
+        className=" font-MTNIrancell-Bold text-xl text-gray-600">
+        {lectureData.lectureTitle}
+      </h3>
+      <div
+        className="w-full h-96 rounded-xl overflow-hidden bg-gray-600">
+        <video
+          controls
+          className=" w-full h-full object-contain rounded-lg"
+          key={lectureData.lectureId}>
+          <source src={lectureData.lectureUrl} type="video/mp4" className=" rounded-lg" />
+        </video>
+      </div>
+    </div>
   )
 }
 
