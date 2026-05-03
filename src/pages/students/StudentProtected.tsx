8d0c3077caae98859a994f8ae0989aee0e667556
Jahan <you@example.com>
2026-05-03 17:08:42 +0330

    [UPDATE] : simulate condition for StudentProtected Page

--- src/pages/students/StudentProtected.tsx
+++ src/pages/students/StudentProtected.tsx
@@ -1,9 +1,16 @@
+import { Outlet, useNavigate } from "react-router-dom"
 
 
 const StudentProtected = () => {
-  return (
-    <div>StudentProtected</div>
-  )
+
+  const navigate = useNavigate()
+
+  // simulate logged in 
+  const isLoggedIn = true
+
+  if (!isLoggedIn) navigate('/students-login')
+
+  return <Outlet />
 }
 
 export default StudentProtected
