553eb315648c963f0ae1850453d179883adb6389
Jahan <you@example.com>
2026-05-28 11:49:09 +0330

    [UPDATE] add Loading logic to StudentLayout

--- src/pages/students/StudentLayout.tsx
+++ src/pages/students/StudentLayout.tsx
@@ -2,13 +2,18 @@ import { Outlet } from 'react-router-dom'
 import { type ReactNode } from 'react'
 import Navbar from '../../components/students/Navbar'
 import Footer from '../../components/students/Footer'
+import useAuth from '../../hooks/useAuth'
+import Loading from '../../components/students/Loading'
 
 
 const StudentLayout = (): ReactNode => {
+
+    const { loading } = useAuth()
+
     return (
         <>
             <Navbar />
-            <Outlet />
+            {loading ? <Loading /> : <Outlet />}
             <Footer />
         </>
     )
