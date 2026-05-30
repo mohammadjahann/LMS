53fcb656e0eb649a8d725d1120637df639ed8eb5
Jahan <you@example.com>
2026-05-30 23:14:05 +0330

    [UPDATE] add Provider to StudentLayout

--- src/pages/students/StudentLayout.tsx
+++ src/pages/students/StudentLayout.tsx
@@ -4,6 +4,8 @@ import Navbar from '../../components/students/Navbar'
 import Footer from '../../components/students/Footer'
 import useAuth from '../../hooks/useAuth'
 import Loading from '../../components/students/Loading'
+import { Provider } from 'react-redux'
+import studentStore from '../../redux/student/studentStore'
 
 
 const StudentLayout = (): ReactNode => {
@@ -13,7 +15,7 @@ const StudentLayout = (): ReactNode => {
     return (
         <>
             <Navbar />
-            {loading ? <Loading /> : <Outlet />}
+            {loading ? <Loading /> : <Provider store={studentStore}><Outlet /></Provider>}
             <Footer />
         </>
     )
