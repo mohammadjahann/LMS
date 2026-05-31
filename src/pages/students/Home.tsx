c0f69388eddd5ab92daca944dbf8d95c27ee0d9f
Jahan <you@example.com>
2026-05-31 11:26:42 +0330

    [ADD] fetching data in Home page by defined asyncthunk in redux toolkit

--- src/pages/students/Home.tsx
+++ src/pages/students/Home.tsx
@@ -1,21 +1,32 @@
+import { useEffect } from "react"
 import CallToAction from "../../components/students/CallToAction"
 import Companies from "../../components/students/Companies"
 import CouresSection from "../../components/students/CouresSection"
 import Hero from "../../components/students/Hero"
+import { useAppDispatch } from "../../redux/student/hooks"
+import { fetchProducts } from "../../redux/student/productSlice"
 
 
 
 const Home = () => {
 
 
+  const dispatch = useAppDispatch()
+
+
+  useEffect(() => {
+    dispatch(fetchProducts())
+  }, [])
+
+
+
   return (
     <div
       className="flex flex-col items-center  text-center">
-
       <Hero />
       <Companies />
-      <CouresSection/>
-      <CallToAction/>
+      <CouresSection />
+      <CallToAction />
 
     </div>
   )
