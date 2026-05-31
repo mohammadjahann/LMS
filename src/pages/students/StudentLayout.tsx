915282cf729548ed707370be7a5495fc8b6ccf43
Jahan <you@example.com>
2026-05-31 20:44:58 +0330

    [UPDATE] adding ToastContainer to StudentLayout

--- src/pages/students/StudentLayout.tsx
+++ src/pages/students/StudentLayout.tsx
@@ -6,6 +6,7 @@ import useAuth from '../../hooks/useAuth'
 import Loading from '../../components/students/Loading'
 import { Provider } from 'react-redux'
 import studentStore from '../../redux/student/studentStore'
+import { ToastContainer } from 'react-toastify';
 
 
 const StudentLayout = (): ReactNode => {
@@ -15,7 +16,7 @@ const StudentLayout = (): ReactNode => {
     return (
         <>
             <Navbar />
-            {loading ? <Loading /> : <Provider store={studentStore}><Outlet /></Provider>}
+            {loading ? <Loading /> : <Provider store={studentStore}> <ToastContainer rtl /><Outlet /></Provider>}
             <Footer />
         </>
     )
