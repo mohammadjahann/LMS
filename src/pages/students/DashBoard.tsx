7cf9f3e391b3035534c24edddff4fe04522ec1bd
Jahan <you@example.com>
2026-06-06 12:51:07 +0330

    [UPDATE] initial design for DashBoard

--- src/pages/students/DashBoard.tsx
+++ src/pages/students/DashBoard.tsx
@@ -1,8 +1,22 @@
+import { Outlet } from "react-router-dom"
 
 
 const DashBoard = () => {
   return (
-    <div>DashBoard</div>
+    <div
+      className="dir w-full h-screen bg-gradient-to-b from-cyan-100 to-cyan-50">
+
+      <div className=" px-4 sm:px-10 md:px-14 lg:px-36 flex flex-col md:flex-row h-full">
+
+        {/* DashBoard nav */}
+        <div >
+          nav
+        </div>
+
+        <Outlet />
+      </div>
+
+    </div>
   )
 }
 
