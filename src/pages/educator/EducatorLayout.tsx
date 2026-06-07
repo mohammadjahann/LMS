91d0dba5dac5a57afc409bbea1e169ce053aa2a9
Jahan <you@example.com>
2026-06-07 10:43:50 +0330

    [FIX] fix base codes of EducatorLayout

--- src/pages/educator/EducatorLayout.tsx
+++ src/pages/educator/EducatorLayout.tsx
@@ -2,15 +2,13 @@ import { Outlet } from "react-router-dom"
 import EducatorNavBar from "../../components/educator/NavBar"
 import EducatorFooter from "../../components/educator/EducatorFooter"
 
- 
 
 const EducatorLayout = () => {
   return (
     <>
-    <EducatorNavBar/>
-    <div>EducatorLayout </div>
-    {<Outlet/>}
-    <EducatorFooter/>
+      <EducatorNavBar />
+      {<Outlet />}
+      <EducatorFooter />
     </>
   )
 }
