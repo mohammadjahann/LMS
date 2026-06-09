8010290a4df2fff28fe0db0791e3bc325cba71a6
Jahan <you@example.com>
2026-06-09 23:06:46 +0330

    [ADD] Define EducatorNavBar and use it in EducatorProtected

--- src/components/educator/EducatorProtected.tsx
+++ src/components/educator/EducatorProtected.tsx
@@ -1,12 +1,18 @@
 import { Navigate, Outlet } from "react-router-dom"
 import useEducatorAuth from "../../hooks/useEducatorAuth"
+import EducatorNavBar from "./NavBar"
 
 
 const EducatorProtected = () => {
 
     const { educatorData } = useEducatorAuth()
 
-    return educatorData ? <Outlet /> : <Navigate to={'educator-login'} replace />
+    return educatorData ? (
+        <>
+            <EducatorNavBar />
+            <Outlet />
+        </>
+    ) : <Navigate to={'educator-login'} replace />
 
 }
 
--- src/components/educator/NavBar.tsx
+++ src/components/educator/NavBar.tsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom";
+import { assets } from "../../assets/assets";
+import useEducatorAuth from "../../hooks/useEducatorAuth"
+import { TbLogout2 } from "react-icons/tb";
 
 
 const EducatorNavBar = () => {
+
+  const { logout } = useEducatorAuth()
+
   return (
-    <div>EducatorNavBar</div>
+    <div className=" w-full px-4 sm:px-10 md:px-14 lg:px-36 bg-gradient-to-b from-cyan-100  to-cyan-50 py-4 flex items-center justify-between dir font-MTNIrancell-Medium">
+
+      {/* Logo */}
+      <Link to={'/'}>
+        <img src={assets.logo} alt="" className="w-28" />
+      </Link>
+
+      <TbLogout2
+        onClick={logout}
+        className=" text-3xl cursor-pointer text-slate-700 hover:text-slate-900 transition-all duration-100" />
+
+    </div>
   )
 }
 
