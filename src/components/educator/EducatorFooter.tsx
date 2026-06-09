6f3920d5a5dc27ca57a69aa6b8743038b6c9b16b
Jahan <you@example.com>
2026-06-09 18:07:05 +0330

    [ADD] Define initial EducatorFooter

--- src/components/educator/EducatorFooter.tsx
+++ src/components/educator/EducatorFooter.tsx
@@ -1,9 +1,51 @@
-
+import { Link } from "react-router-dom";
+import { assets } from "../../assets/assets";
+import { FaInstagram, FaGithub } from "react-icons/fa";
 
 const EducatorFooter = () => {
   return (
-    <div>EducatorFooter</div>
-  )
-}
+    <footer
+      className="
+      dir w-full bg-gray-900 px-4 sm:px-10 md:px-14 lg:px-36 py-6 grid  grid-cols-1 md:grid-cols-3 items-center gap-6">
+      {/* Logo */}
+      <Link
+        to="/"
+        className="justify-self-center md:justify-self-start"
+      >
+        <img
+          src={assets.logo_dark}
+          alt="Logo"
+          className="w-36"
+        />
+      </Link>
 
-export default EducatorFooter
+      {/* Social */}
+      <div
+        className=" flex justify-center items-center gap-4">
+        <a
+          href="https://www.instagram.com/mohammadjahann/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaInstagram className="text-white text-3xl hover:text-cyan-100 transition" />
+        </a>
+
+        <a
+          href="https://github.com/mohammadjahann"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub className="text-white text-3xl hover:text-cyan-100 transition" />
+        </a>
+      </div>
+
+      {/* Copyright */}
+      <p
+        className=" text-white/60 text-xs md:text-sm text-center md:text-left md:justify-self-end ">
+        Copyright 2026 - Design with ❤ by Jahan
+      </p>
+    </footer>
+  );
+};
+
+export default EducatorFooter;
