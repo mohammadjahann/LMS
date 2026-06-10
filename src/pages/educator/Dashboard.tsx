1c39029b2268cc7dcb71c7a892db55874b4bc3b2
Jahan <you@example.com>
2026-06-10 17:48:43 +0330

    [ADD] Define educator Dashboard Page

--- src/pages/educator/Dashboard.tsx
+++ src/pages/educator/Dashboard.tsx
@@ -1,8 +1,29 @@
+import { Outlet } from "react-router-dom"
+import EducatorDashBoardNav from "../../components/educator/EducatorDashBoardNav";
+
+
 
 
 const Dashboard = () => {
+
+
+
   return (
-    <div>Dashboard</div>
+    <div
+      className=" relative dir min-h-screen bg-gradient-to-br from-slate-50 via-cyan-50 to-blue-50 py-8">
+
+      <div
+        className="w-[95%] xl:w-[85%] mx-auto flex flex-col md:flex-row gap-6 ">
+
+        <EducatorDashBoardNav />
+        <div
+          className="flex-1 bg-white/80 backdrop-blur-xl rounded-3xl border border-white shadow-[0_10px_40px_rgba(0,0,0,0.08)] p-6">
+          <Outlet />
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
