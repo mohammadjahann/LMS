f8e520d108ac57aea0cc57f32e5de5db70cb8da6
Jahan <you@example.com>
2026-06-12 17:53:07 +0330

    [UPDATE] Add new styles

--- src/components/educator/EducatorPanelBox.tsx
+++ src/components/educator/EducatorPanelBox.tsx
@@ -4,22 +4,22 @@ type Props = {
     children: ReactNode
     styles: string,
     title: string,
-    data: number
+    data: string | number
 }
 
 
 const EducatorPanelBox = ({ children, styles, title, data }: Props) => {
     return (
         <div
-            className={`border rounded-3xl p-6 ${styles}`}>
+            className={`border rounded-3xl font-MTNIrancell-Medium p-6 flex flex-col items-center md:items-start ${styles}`}>
 
             {children}
 
-            <h3 className="mt-4 text-slate-500">
+            <h3 className="mt-4 text-sm md:text-[18px] text-slate-500">
                 {title}
             </h3>
 
-            <p className=" text-4xl font-bold mt-3 text-slate-800">
+            <p className=" text-2xl md:text-3xl font-bold mt-3 text-slate-800">
                 {data}
             </p>
 
