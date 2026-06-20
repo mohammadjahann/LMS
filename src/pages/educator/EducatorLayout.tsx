73133cc743a28c516137c9e8afe890c1db627553
Jahan <you@example.com>
2026-06-20 16:45:28 +0330

    [ADD] Add ToastContainer in EducatorLayout

--- src/pages/educator/EducatorLayout.tsx
+++ src/pages/educator/EducatorLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import EducatorFooter from "../../components/educator/EducatorFooter"
 import { EducatorAuthContextProvider } from "../../context/EducatorAuthContext"
+import { ToastContainer } from "react-toastify"
 
 
 const EducatorLayout = () => {
   return (
     <>
       <EducatorAuthContextProvider>
+        <ToastContainer />
         <Outlet />
       </EducatorAuthContextProvider>
       <EducatorFooter />
