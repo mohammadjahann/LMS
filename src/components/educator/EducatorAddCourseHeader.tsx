87e3cf5f6c1c5941e5e71a5b843a9ab0cd892b16
Jahan <you@example.com>
2026-06-29 22:10:12 +0330

    [UPDATE] Make EducatorAddCourseHeader component responsive

--- src/components/educator/EducatorAddCourseHeader.tsx
+++ src/components/educator/EducatorAddCourseHeader.tsx
@@ -1,25 +1,24 @@
-import React from 'react'
 
 const EducatorAddCourseHeader = () => {
     return (
         <div
-            className=" mb-8 flex flex-col lg:flex-row justify-between gap-5">
+            className=" mb-8 flex flex-col lg:flex-row justify-between items-center gap-5">
 
             <div>
 
-                <h1 className="text-3xl font-MTNIrancell-Bold text-slate-800">
+                <h1 className="text-xl text-center md:text-3xl md:text-right font-MTNIrancell-Bold text-slate-800">
                     افزودن دوره جدید
                 </h1>
 
                 <p
-                    className="text-slate-500 mt-2">
+                    className="text-slate-500 mt-2 text-center md:text-right text-[12px] md:text-[16px] ">
                     اطلاعات دوره، فصل‌ها و جلسات را ثبت کنید
                 </p>
 
             </div>
 
             <div
-                className="px-5 py-3 rounded-2xl bg-amber-50 border border-amber-200 text-amber-700">
+                className=" px-5 py-3 rounded-2xl bg-amber-50 border border-amber-200 text-amber-700">
                 وضعیت: پیش نویس
             </div>
 
